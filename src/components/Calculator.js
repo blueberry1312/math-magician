6a397d1beab45bd41b7bf0a46b56c496977a14f2
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-03-23 21:13:41 -0300

    Revert "Add logic to calculator component"
    This reverts commit aa0ccdee742deb9478192443964d9cac063db708.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Calculator.css';
-import calculate from '../logic/calculate';
 
 export default function Calculator() {
-  const [state, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-
-  const handleClick = (buttonName) => {
-    const newState = calculate(state, buttonName);
-    setState(newState);
-  };
-
+  const result = 0;
   return (
     <div className="container">
-      <div className="result">{state.next || state.total || '0'}</div>
-      <button type="button" className="btn-light btn-zero" onClick={() => handleClick('0')}>0</button>
-      <button type="button" className="btn-light btn-dot" onClick={() => handleClick('.')}>.</button>
-      <button type="button" className="btn-coloured btn-equal" onClick={() => handleClick('=')}>=</button>
-      <button type="button" className="btn-light btn-clear" onClick={() => handleClick('AC')}>AC</button>
-      <button type="button" className="btn-light btn-sign" onClick={() => handleClick('+/-')}>+/-</button>
-      <button type="button" className="btn-light btn-percentage" onClick={() => handleClick('%')}>%</button>
-      <button type="button" className="btn-coloured btn-divider" onClick={() => handleClick('÷')}>/</button>
-      <button type="button" className="btn-coloured btn-multiply" onClick={() => handleClick('x')}>*</button>
-      <button type="button" className="btn-coloured btn-substract" onClick={() => handleClick('-')}>-</button>
-      <button type="button" className="btn-coloured btn-plus" onClick={() => handleClick('+')}>+</button>
-      <button type="button" className="btn-light btn-one" onClick={() => handleClick('1')}>1</button>
-      <button type="button" className="btn-light btn-two" onClick={() => handleClick('2')}>2</button>
-      <button type="button" className="btn-light btn-three" onClick={() => handleClick('3')}>3</button>
-      <button type="button" className="btn-light btn-four" onClick={() => handleClick('4')}>4</button>
-      <button type="button" className="btn-light btn-five" onClick={() => handleClick('5')}>5</button>
-      <button type="button" className="btn-light btn-six" onClick={() => handleClick('6')}>6</button>
-      <button type="button" className="btn-light btn-seven" onClick={() => handleClick('7')}>7</button>
-      <button type="button" className="btn-light btn-eight" onClick={() => handleClick('8')}>8</button>
-      <button type="button" className="btn-light btn-nine" onClick={() => handleClick('9')}>9</button>
+      <div className="result">{result}</div>
+      <button type="button" className="btn-light btn-zero">0</button>
+      <button type="button" className="btn-light btn-dot">.</button>
+      <button type="button" className="btn-coloured btn-equal">=</button>
+      <button type="button" className="btn-light btn-clear">AC</button>
+      <button type="button" className="btn-light btn-sign">+/-</button>
+      <button type="button" className="btn-light btn-percentage">%</button>
+      <button type="button" className="btn-coloured btn-divider">/</button>
+      <button type="button" className="btn-coloured btn-multiply">*</button>
+      <button type="button" className="btn-coloured btn-substract">-</button>
+      <button type="button" className="btn-coloured btn-plus">+</button>
+      <button type="button" className="btn-light btn-one">1</button>
+      <button type="button" className="btn-light btn-two">2</button>
+      <button type="button" className="btn-light btn-three">3</button>
+      <button type="button" className="btn-light btn-four">4</button>
+      <button type="button" className="btn-light btn-five">5</button>
+      <button type="button" className="btn-light btn-six">6</button>
+      <button type="button" className="btn-light btn-seven">7</button>
+      <button type="button" className="btn-light btn-eight">8</button>
+      <button type="button" className="btn-light btn-nine">9</button>
     </div>
   );
 }
