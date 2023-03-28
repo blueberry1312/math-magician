ab1d7db8cd443766f6c354b6578e6ca0b95d37dd
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-03-28 16:04:48 -0300

    Create route and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
-import './App.css';
 import Quote from './components/quote';
+import Home from './components/Home';
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Math Magicians</h1>
+      <h2>Welcome to our page!</h2>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quote />
-    </div>
+    <Router>
+      <div className="firstcontainer">
+        <div className="navbar">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/calculator">Calculator</Link>
+              </li>
+              <li>
+                <Link to="/quote">Quote</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quote" element={<Quote />} />
+          </Routes>
+        </main>
+
+        <footer className="footer">
+          <p>Copyright © 2023 Made by Nestor, logic by microverse.</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="section home">
+      <p>
+        Welcome to our calculator webapp! Our app allows you to perform basic arithmetic
+        operations such as addition, subtraction, multiplication, and division. Its simple
+        and easy to use, perfect for anyone who needs a quick and reliable calculator tool.
+        Try it out now and see how it can make your calculations a breeze!
+      </p>
+    </div>
+  );
+}
